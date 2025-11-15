26e1905df6f13e9d225e1a37d87fb2c274834963
ujjawal06-star <ujjwal9197us@gmail.com>
2025-11-15 22:43:38 +0530

    Reverse Only Letters 917 leet

---
+++ revonlylett917.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0, j = s.size() - 1;
+
+        while (i < j) {
+            if (!isalpha(s[i])) {
+                i++;
+            } 
+            else if (!isalpha(s[j])) {
+                j--;
+            }
+            else {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "a-bC-dEf-ghIj"; 
+    cout << sol.reverseOnlyLetters(s);
+    return 0;
+}
