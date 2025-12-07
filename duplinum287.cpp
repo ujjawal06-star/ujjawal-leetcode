c5ac92ff2d36cd2a3ce85865fbd59017f19f9e67
ujjawal06-star <ujjwal9197us@gmail.com>
2025-12-07 22:11:39 +0530

    Find the Duplicate Number 287 leet

---
+++ duplinum287.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    Solution obj;
+    cout << obj.findDuplicate(nums);
+    return 0;
+}
